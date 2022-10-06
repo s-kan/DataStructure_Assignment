d206d8c6872ec950724216eef8c0efdc0d0557d1
s-kan <gkswltjr00@gmail.com>
2022-10-06 17:32:46 +0900

    pa4-modification

--- pa4/test.py
+++ pa4/test.py
@@ -1,44 +1,43 @@
 import sys
+
 K, N = map(int, sys.stdin.readline().split())
 arr = []
-slot = []
 for i in range(N):
     arr.append(int(sys.stdin.readline()))
-for i in range(K):
-    slot.append([0,0])
+slot = [0]*K
 
 size = K
 c = 0
 for i in range(N):
     if arr[i] > 0:
-        if c == size:
-            size = size * 2
-            for j in range(c, size):
-                slot.append([0, 0])
-            slot[c][0], slot[c][1] = c + 1, arr[i]
-            c = c + 1
-        else:
+        if c != size:
             for j in range(size):
-                if slot[j][0] == 0:
-                    slot[j][0], slot[j][1] = j+1, arr[i]
+                if slot[j] == 0:
+                    slot[j] = arr[i]
                     c = c+1
                     break
+        else:
+            size = size * 2
+            for j in range(c, size):
+                slot.append(0)
+            slot[c] = arr[i]
+            c = c + 1
     elif arr[i] < 0:
         a = abs(arr[i])
         for j in range(size):
-            if slot[j][0] != 0 and slot[j][1] == a:
-                slot[j][0], slot[j][1], c = 0, 0, c-1
+            if slot[j] != 0 and slot[j] == a:
+                slot[j], c = 0, c-1
                 break
         if c <= size//3 and size != K:
             l = 1
             for x in range(size):
-                if slot[x][0] != 0:
+                if slot[x] != 0:
                     if x != l-1:
-                        slot[l - 1][0], slot[l - 1][1], slot[x][0], slot[x][1] = l, slot[x][1], 0, 0
+                        slot[l - 1], slot[x] = slot[x], 0
                     l = l+1
             del slot[size//2:]
             size = size//2
 
 for i in range(size):
-    if slot[i][0] != 0:
-        print(slot[i][0], slot[i][1])
+    if slot[i] != 0:
+        print(i+1, slot[i])
